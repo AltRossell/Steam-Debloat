94cacf04545a0160758c188c5f8633f1c58c8779
rossell <168254237+AltRossell@users.noreply.github.com>
2025-08-03 15:55:08 -0300

    Delete docs/script/Installer2022_2025.bat

--- docs/script/Installer2022_2025.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-set url='https://raw.githubusercontent.com/mtytyx/Steam-Debloat/main/docs/script/app_full_2022_2025_dev.ps1'
-set tls=[Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12;
-
-%SYSTEMROOT%\System32\WindowsPowerShell\v1.0\powershell.exe ^
--Command %tls% " & { $(try { iwr -useb %url% } catch { iwr -useb %url% })} | iex
-
-exit /b
