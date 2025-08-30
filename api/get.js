bd88d6b2230576333429b1954de67eccc02a7601
rossell <168254237+AltRossell@users.noreply.github.com>
2025-08-30 13:22:14 -0300

    Update get.js

--- api/get.js
+++ api/get.js
@@ -1,9 +1,7 @@
 export default async function handler(req, res) {
   try {
-    // URL raw de tu script PowerShell en GitHub
     const url = "https://raw.githubusercontent.com/AltRossell/Steam-Debloat/main/api/menu.ps1";
     
-    // fetch nativo de Node (Vercel soporta fetch global)
     const response = await fetch(url);
     
     if (!response.ok) {
@@ -12,7 +10,6 @@ export default async function handler(req, res) {
 
     const script = await response.text();
 
-    // Configuramos la respuesta como texto plano
     res.setHeader("Content-Type", "text/plain");
     res.status(200).send(script);
   } catch (error) {
@@ -20,3 +17,4 @@ export default async function handler(req, res) {
     res.status(500).send("Error interno del servidor");
   }
 }
+
