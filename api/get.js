74848e7b3ab49945a530f57177b1b62d9a7b7e8b
rossell <168254237+AltRossell@users.noreply.github.com>
2025-09-15 00:05:27 -0300

    Update get.js

--- api/get.js
+++ api/get.js
@@ -1,7 +1,7 @@
 export default async function handler(req, res) {
 	try {
 		const url =
-			"https://raw.githubusercontent.com/AltRossell/Steam-Debloat/legacy-scripts/app.ps1";
+			"https://raw.githubusercontent.com/AltRossell/Steam-Debloat/legacy-scripts-archived/app.ps1";
 
 		const response = await fetch(url);
 
